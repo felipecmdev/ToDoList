7aa25dd51757cf97ccabb0c879d0403413a44133
Felipe <felipecmoura30@gmail.com>
2024-07-17 22:18:35 -0300

    primeito commit

---
+++ Projeto/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace Projeto
+{
+    internal class Tarefas
+    {
+        public List<string> Log { get; set; }
+
+        public Tarefas()
+        {
+            Log = new List<string>();
+        }
+
+        public void ListarTarefas()
+        {
+            if (Log.Count == 0)
+            {
+                Console.WriteLine("A lista está vazia.");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                foreach (string lista in Log)
+                {
+                    Console.WriteLine(lista);
+                    Console.WriteLine();
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        public void SalvarTarefas()
+        {
+            if (Log.Count == 0)
+            {
+                Console.WriteLine("A lista está vazia.");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                string caminho = @"D:\codes\C#\Primeiro_Projeto\ListaDeAfazeres.txt";
+
+                using (StreamWriter sw = new StreamWriter(caminho))
+                {
+                    DateTime now = DateTime.Now;
+                    sw.WriteLine(now.ToShortDateString());
+                    sw.WriteLine("----------");
+                    foreach (string tarefas in Log)
+                    {
+                        sw.WriteLine(tarefas);
+                        sw.WriteLine();
+                    }
+                }
+            }
+        }
+
+        public void RemoverTarefa()
+        {
+            if (Log.Count == 0)
+            {
+                Console.WriteLine("A lista está vazia.");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                Log.RemoveRange(Log.Count - 2, 2);
+            }
+        }
+    }
+}
---
+++ Projeto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Projeto
+{
+    internal class Menu
+    {
+        static void Main()
+        {
+            Tarefas tarefas = new Tarefas();
+            while (true)
+            {
+                Console.WriteLine("O que deseja fazer:");
+                string opcao = Console.ReadLine();
+                Console.Clear();
+
+                if (opcao == "adicionar tarefa" || opcao == "adicionar tarefas")
+                {
+                    Console.WriteLine("Escreva sua tarefa do dia e sua descrição: ");
+                    string titulo = Console.ReadLine().ToUpper();
+                    tarefas.Log.Add(titulo);
+
+                    Console.WriteLine();
+                    
+                    string descricao = Console.ReadLine();
+                    tarefas.Log.Add(descricao);
+                    Console.Clear();
+                }
+                else if (opcao == "remover tarefa")
+                {
+                    tarefas.RemoverTarefa();
+                    Console.WriteLine("Última tarefa apagadas com sucesso");
+                    Console.Read();
+                    Console.Clear();
+                }
+                else if (opcao == "listar tarefas" || opcao == "listar tarefa")
+                {
+                    tarefas.ListarTarefas();
+                }
+                else if (opcao == "salvar tarefas" || opcao == "salvar tarefa")
+                {
+                    tarefas.SalvarTarefas();
+                    Console.WriteLine();
+                    break;
+                }
+            }
+            Console.WriteLine("Tarefas salvas com sucesso!");
+            Console.WriteLine();
+        }
+    }
+}
